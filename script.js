b1f99bfd64f751c4898ff06d10a763f275fbdbc1
Bruna <99213174+BrunaMonteiro2@users.noreply.github.com>
2023-10-18 22:56:23 -0300

    Atividade 10

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-function encontrarMaiorNumero(num1, num2, num3, num4) {
-    return Math.max(num1, num2, num3, num4);
-}
+function verificarTriangulo() {
+    var a = parseFloat(document.getElementById('a').value);
+    var b = parseFloat(document.getElementById('b').value);
+    var c = parseFloat(document.getElementById('c').value);
 
-function ordenarNumerosCrescente(num1, num2, num3, num4) {
-    const numeros = [num1, num2, num3, num4];
-    numeros.sort(function(a, b) {
-        return a - b;
-    });
-    return numeros;
+    if (isNaN(a) || isNaN(b) || isNaN(c)) {
+        document.getElementById('resultado').innerHTML = 'Por favor, insira números válidos.';
+        return;
+    }
+
+    if ((Math.abs(b - c) < a && a < b + c) &&
+        (Math.abs(a - c) < b && b < a + c) &&
+        (Math.abs(a - b) < c && c < a + b)) {
+
+        if (a === b && b === c) {
+            document.getElementById('resultado').innerHTML = 'O triângulo é equilátero.';
+        } else if (a === b || b === c || a === c) {
+            document.getElementById('resultado').innerHTML = 'O triângulo é isósceles.';
+        } else {
+            document.getElementById('resultado').innerHTML = 'O triângulo é escaleno.';
+        }
+
+    } else {
+        document.getElementById('resultado').innerHTML = 'Os valores não podem formar um triângulo.';
+    }
 }
